2c07b0fab837b595c222baccef48704b1a001e7f
mininerej <ejhong.4991@gmail.com>
2019-05-26 11:35:33 +0900

    :sparkles: add interface to get a list of posts

---
+++ models/post.js
@@ -0,0 +1,9 @@
+exports.getPostList = lastPostId => {};
+
+exports.createPost = post => {};
+
+exports.getPost = postId => {};
+
+exports.updatePost = post => {};
+
+exports.removePost = postId => {};
--- routes/posts.js
+++ routes/posts.js
@@ -1,8 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const model = require('../models/post');
+const errors = require('../utils/errors');
 
 const getPostList = (request, response, next) => {
-  response.send('get a list of posts');
+  const category = request.query['category'];
+  const lastPostId = parseInt(request.query['last-post-id']) || undefined;
+  try {
+    const postList = model.getPostList(category, lastPostId);
+    response.setHeader('Content-Type', 'application/json');
+    response.send(JSON.stringify(postList));
+  } catch (error) {
+    if (error.message === errors.INDEX_EXCEED_RANGE) {
+      response.setHeader('Content-Type', 'text/html');
+      response.statusCode = 404;
+      response.send('Index exceeded range.');
+    } else if (error.message === errors.CATEGORY_DO_NOT_EXIST) {
+      response.setHeader('Content-Type', 'text/html');
+      response.statusCode = 404;
+      response.send(`We don't support category ${category}`);
+    } else {
+      response.send(error);
+    }
+  }
 };
 
 const createPost = (request, response, next) => {
