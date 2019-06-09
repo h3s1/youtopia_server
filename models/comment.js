abfc73fb6db59913af9bede3e398d8fdd5352cff
mininerej <ejhong.4991@gmail.com>
2019-06-09 11:20:07 +0900

    feat: add comment model

---
+++ models/comment.js
@@ -0,0 +1,42 @@
+const Sequelize = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  const comment = sequelize.define(
+    'Comment',
+    {
+      id: {
+        type: Sequelize.INTEGER.UNSIGNED,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      content: {
+        type: Sequelize.TEXT('long')
+      },
+      // eslint-disable-next-line
+      article_id: {
+        type: Sequelize.INTEGER.UNSIGNED
+      },
+      // eslint-disable-next-line
+      author_id: {
+        type: Sequelize.STRING
+      }
+    },
+    {
+      tableName: 'comment',
+      freezeTableName: true,
+      charset: 'utf8mb4',
+      collate: 'utf8mb4_general_ci'
+    }
+  );
+  comment.associate = models => {
+    comment.belongsTo(models.User, {
+      foreignKey: 'author_id',
+      targetKey: 'user_id'
+    });
+    comment.belongsTo(models.Article, {
+      foreignKey: 'article_id',
+      targetKey: 'id'
+    });
+  };
+  return comment;
+};
