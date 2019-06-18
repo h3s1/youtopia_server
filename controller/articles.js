e9f7a5bb2329b354386352e33045857b53c0474c
mininerej <ejhong.4991@gmail.com>
2019-06-18 21:40:32 +0900

    fix: create article

--- controller/articles.js
+++ controller/articles.js
@@ -17,19 +17,34 @@ const getArticleList = async (request, response, next) => {
   }
 };
 
-const createArticle = (request, response, next) => {
-  body = request.body;
-  try {
-    model.createArticle({
-      title: body.title,
-      content: body.content,
-      videoId: body.video_id,
-      userId: body.user_id
-    });
-  } catch (error) {
-    response.send(error);
+const createArticle = async (request, response, next) => {
+  if (!request.headers.authorization) {
+    return response.status(403).json({ error: 'No credentials sent!' });
   }
-  response.send('article Uploaded');
+
+  const token = request.headers.authorization.split(' ')[1];
+
+  const articleId = parseInt(request.params.articleId);
+
+  if (typeof token !== 'undefined') {
+    try {
+      const decoded = auth.verify(token);
+      const user = await UserService.findUserById(decoded.userId);
+
+      body = request.body;
+      await model.createArticle({
+        userId: user.user_id,
+        title: body.title,
+        content: body.content,
+        videoId: body.video_id
+      });
+      response.send('article Uploaded');
+    } catch (error) {
+      response.status(400).send(error.message);
+    }
+  } else {
+    res.status(403).send(error.message);
+  }
 };
 
 const getArticle = async (request, response, next) => {
