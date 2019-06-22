55c50270e49dadcfa6b1032f61b7ca690adf7131
mininerej <ejhong.4991@gmail.com>
2019-06-22 16:27:01 +0900

    feat: add recommendation list

--- controller/recommendations.js
+++ controller/recommendations.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const ArticleService = require('../services/article');
 
-const getRecommendationList = (request, response, next) => {
-  response.send('get a list of recommendations');
+const getRecommendationList = async (request, response, next) => {
+  try {
+    const articleList = await ArticleService.getarticleList('hot', 0);
+    response.json(articleList);
+  } catch (error) {
+    response.send(error);
+  }
 };
 
 router.get('/', getRecommendationList);
